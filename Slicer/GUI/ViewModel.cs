c05254db7d520f4fb4b06c29e469274126b23d21
Wosser1sProductions <wosser1@hotmail.com>
2019-10-28 17:22:10 +0100

    Barebone project

---
+++ Slicer/GUI/ViewModel.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Media;
+
+namespace Slicer.GUI
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        Brush _Brush;
+        public Brush Brush
+        {
+            get { return _Brush; }
+            set
+            {
+                _Brush = value;
+                OnPropertyChanged("Brush");
+            }
+        }
+
+        int _valueSetting1 = 500;
+        public int ValueSetting1
+        {
+            get { return _valueSetting1; }
+            set
+            {
+                _valueSetting1 = value;
+                OnPropertyChanged("ValueSetting1");
+            }
+        }
+
+        int _MeshSizeU = 140;
+        public int MeshSizeU
+        {
+            get { return _MeshSizeU; }
+            set
+            {
+                _MeshSizeU = value;
+                OnPropertyChanged("MeshSizeU");
+            }
+        }
+
+        int _MeshSizeV = 140;
+        public int MeshSizeV
+        {
+            get { return _MeshSizeV; }
+            set
+            {
+                _MeshSizeV = value;
+                OnPropertyChanged("MeshSizeV");
+            }
+        }
+
+        double _ParameterW = 1;
+        public double ParameterW
+        {
+            get { return _ParameterW; }
+            set
+            {
+                _ParameterW = value;
+                OnPropertyChanged("ParameterW");
+            }
+        }
+
+        string _ModelTitle;
+        public string ModelTitle
+        {
+            get { return _ModelTitle; }
+            set
+            {
+                _ModelTitle = value;
+                OnPropertyChanged("ModelTitle");
+            }
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ Slicer/MainWindow.xaml.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Resources;
+using System.Windows.Shapes;
+using HelixToolkit.Wpf;
+using Microsoft.Win32;
+using Slicer.GUI;
+using Path = System.IO.Path;
+
+namespace Slicer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly ViewModel _viewModel = new ViewModel();
+
+        private readonly ClonedVisual3D surface2;
+        private bool _isInvalidated = true;
+        private bool _isUpdating;
+        private object updateLock = "abc";
+        private UIElement currentView;
+        private Viewport3D v1;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            CompositionTarget.Rendering += this.OnCompositionTargetRendering;
+            DataContext = _viewModel;
+
+            _viewModel.PropertyChanged += ModelChanged;
+
+            surface2 = new ClonedVisual3D();
+
+            _viewModel.Brush = Brushes.Blue;
+
+            //viewport.Viewport.Children.Add(new DefaultLights());
+
+            v1 = viewport.Viewport;
+            currentView = viewport;
+
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Load(path);
+        }
+
+        private void OnCompositionTargetRendering(object sender, EventArgs e)
+        {
+            if (_isInvalidated)
+            {
+                // sync:
+                // _isInvalidated = false;
+                // UpdateModel(source1.Text);
+
+                // async:
+                BeginUpdateModel();
+            }
+        }
+
+        private void ModelChanged(object sender, PropertyChangedEventArgs e)
+        {
+            Invalidate();
+        }
+
+        private void Load(Uri uri)
+        {
+            try
+            {
+                StreamResourceInfo sri = Application.GetResourceStream(uri);
+                Load(sri.Stream);
+            }
+            catch
+            {
+                MessageBox.Show("Cannot read model");
+            }
+        }
+
+        private void Load(string p)
+        {
+            var s = new FileStream(p, FileMode.Open);
+            Load(s);
+            s.Close();
+            _viewModel.ModelTitle = Path.GetFileNameWithoutExtension(p);
+        }
+
+        private void Load(Stream s)
+        {
+            Invalidate();
+
+            // todo: binding didn't work
+            viewport.Title = _viewModel.ModelTitle;
+            viewport.CameraController.ResetCamera();
+        }
+
+        private void Invalidate()
+        {
+            lock (updateLock)
+            {
+                _isInvalidated = true;
+            }
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void BeginUpdateModel()
+        {
+            lock (updateLock)
+            {
+                if (!_isUpdating)
+                {
+                    _isInvalidated = false;
+                    _isUpdating = true;
+                    //Dispatcher.Invoke(new Action<string>(UpdateModel), src);
+                }
+            }
+        }
+
+        private void UpdateModel(string src)
+        {
+            //UpdateSurface(surface1, src);
+            _isUpdating = false;
+        }
+
+        private void UpdateSurface(Mesh3D surface1, string src)
+        {
+            //surface1.Source = null;
+            //surface1.MeshSizeU = _viewModel.MeshSizeU;
+            //surface1.MeshSizeV = _viewModel.MeshSizeV;
+            //surface1.ParameterW = _viewModel.ParameterW;
+
+            //// now the surface should be updated
+            //surface1.Source = src;
+        }
+
+        private void ViewSettings_Click(object sender, RoutedEventArgs e)
+        {
+            // todo: move to viewmodel
+            if (ViewSettings.IsChecked)
+            {
+                panelSettings.Visibility = Visibility.Visible;
+                Grid.SetColumn(viewport, 1);
+                Grid.SetColumnSpan(viewport, 1);
+            }
+            else
+            {
+                panelSettings.Visibility = Visibility.Collapsed;
+                Grid.SetColumn(viewport, 0);
+                Grid.SetColumnSpan(viewport, 2);
+            }
+        }
+
+        private void FullScreen_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Normal;
+            WindowStyle = Fullscreen.IsChecked ? WindowStyle.None : WindowStyle.ThreeDBorderWindow;
+            WindowState = Fullscreen.IsChecked ? WindowState.Maximized : WindowState.Normal;
+            ResizeMode = Fullscreen.IsChecked ? ResizeMode.NoResize : ResizeMode.CanResize;
+
+            // mainMenu.Visibility = Fullscreen.IsChecked ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        private void Export_Click(object sender, RoutedEventArgs e)
+        {
+            var d = new SaveFileDialog
+            {
+                Filter = Exporters.Filter,
+                DefaultExt = ".png"
+            };
+
+            if (d.ShowDialog().Value)
+            {
+                string ext = Path.GetExtension(d.FileName).ToLower();
+                viewport.Export(d.FileName);
+                Process.Start(Path.GetDirectoryName(d.FileName));
+            }
+        }
+
+        private void ResetCamera_Click(object sender, RoutedEventArgs e)
+        {
+            viewport.CameraController.ResetCamera();
+        }
+    }
+}
